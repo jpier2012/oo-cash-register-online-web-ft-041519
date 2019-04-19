a3b96f16c8a56758b2c19c9048e3ac9270fcbe61
James Pier <jpier2012@gmail.com>
2019-04-19 12:59:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,13 +1,13 @@
 class CashRegister
 
   attr_reader :total, :discount
-  def initialize(discount = 0)
-    @total = 0
+  def initialize(total = 0, discount = 0)
+    @total = total
     @discount = discount
   end
 
   def add_item(item, price)
-
+    
   end
 
   def total
