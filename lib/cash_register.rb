029f313b2fc0b030ae4b946731cf5ca9af04cc78
James Pier <jpier2012@gmail.com>
2019-04-19 13:28:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
   def apply_discount
     @discount == 0 ? puts "There is no discount to apply." : nil
     @total *= (1 + @discount)
-    "After the discount, the total comes to #{@total}."
+    puts "After the discount, the total comes to #{@total}."
   end
 
   def total
