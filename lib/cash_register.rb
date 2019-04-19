43ec6e21f165f588eefbc6c9dc55a1ab3c8477dc
James Pier <jpier2012@gmail.com>
2019-04-19 13:10:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,10 @@
 class CashRegister
-  @items = []
-  @item_last_scanned = @items[-1]
-
-  attr_reader :total, :discount
+  attr_reader :total, :discount, :items, :item_last_scanned
 
   def initialize(total = 0, discount = 0)
     @total = total
     @discount = discount
+    @items = []
   end
 
   def self.items
@@ -14,7 +12,8 @@ class CashRegister
   end
 
   def add_item(item, price, quantity = 1)
-
+    self.items << item
+    @item_last_scanned = @items[-1]
   end
 
   def apply_discount
