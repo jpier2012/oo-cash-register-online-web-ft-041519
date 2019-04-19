12fdc3394c444ae1393bd90c01b3b9e8de9b8479
James Pier <jpier2012@gmail.com>
2019-04-19 13:14:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,11 +8,13 @@ class CashRegister
   end
 
   def add_item(item, price, quantity = 1)
+    total += price * quantity
     self.items << item
     @item_last_scanned = self.items[-1]
   end
 
   def apply_discount
+    
   end
 
   def total
